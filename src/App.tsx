6bfbe46dae18da25de8976fa3f5453da1697eaea
Athul Philip <athulphilip1309@gmail.com>
2021-04-24 22:28:17 +0100

    :sparkles: Implement app entry point and App component.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Greeting = styled.h1`
+  color: hsl(0, 0%, 100%);
+`;
+
+const App = () => {
+  return <Greeting>Listener</Greeting>;
+};
+
+export default App;
