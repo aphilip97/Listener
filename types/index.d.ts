7e20f44a78d9901bfb1494c2a154988034402202
Athul Philip <athulphilip1309@gmail.com>
2021-04-25 02:44:52 +0100

    :label: Update typings.

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,10 +3,22 @@ type Track = {
   name: string;
 };
 
+export type ExtensionsToMimeTypesMap = {
+  webm: "audio/webm";
+  m4a: "audio/x-m4a";
+}
+
+type Extension = keyof ExtensionsToMimeTypesMap;
+
+export type MimeType = {
+  [K in keyof ExtensionsToMimeTypesMap]: ExtensionsToMimeTypesMap[K]
+}[keyof ExtensionsToMimeTypesMap]
+
 // The file extension is .listen hence the name Listen :)
 export type Listen = {
   title: string;
   src: string;
+  ext: Extension;
   tracklist: Track[];
 };
 
